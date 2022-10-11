1d050c5759f98f1373020652e4270a873a02ac83
Christina Zhivilova <krishakirova@yandex.ru>
2022-10-11 19:28:54 +0300

    Task15

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,13 +1,15 @@
 ﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 Console.Clear();
-Console.WriteLine("Введите число:");
+Console.WriteLine("Введите число от 0 до 100 000:");
 int num = int.Parse(Console.ReadLine()!);
-int x1 = num/10;
+int x1 = 0;
+int x2 = 0;
 
-if (x1>10 && x1<100)
+if (num>100 && num<100000)
 {
-    x2 = x1%10
-    Console.WriteLine($"Третья цифра указанного числа" {x2}");
+    x1 = num/10;
+    x2 = x1%10;
+    Console.WriteLine($"Третья цифра указанного числа{num}->{x2} ");
 }
 else 
 {
---
+++ Task15/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+Console.WriteLine("Введите число от 1 до 7:");
+int x = int.Parse(Console.ReadLine()!);
+
+if (x>=1 && x<6)
+{
+    Console.WriteLine("Будний день");
+}
+else if (x==6 && x==7) 
+{
+    Console.WriteLine("Выходной");
+}
