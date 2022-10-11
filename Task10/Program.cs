35f319c50614a51c4d73e26c48f6dcc1c5ae336a
Christina Zhivilova <krishakirova@yandex.ru>
2022-10-11 18:20:54 +0300

    Добавление Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -3,7 +3,7 @@ Console.Clear();
 Console.WriteLine("Введите трехзначное число:");
 int num = int.Parse(Console.ReadLine()!);
 int x1 = num/10;
-int x2 = num%10;
+int x2 = x1%10;
 Console.WriteLine($"Вторая цифра числа {x2}");
 
 
